11a4ccbf45c946044d42085b7d90fec2bc47dcca
PC201301010007\Administrator <944804675@qq.com>
2016-09-28 17:30:21 +0800

    测试使用urllib库进行文件的上传

--- TestUpload.py
+++ TestUpload.py
@@ -1,10 +1,9 @@
-# from urllib import request, parse
-# # 媒体类型
-# import mimetypes
-# import os
-# # id生成器
-# import uuid
-import requests
+from urllib import request, parse
+# 媒体类型
+import mimetypes
+import os
+# id生成器
+import uuid
 
 """
 	结果: PostMan 不添加header即可post成功
@@ -16,55 +15,46 @@ import requests
 
 """
 
-filePath = r"C:\Users\Administrator\Desktop\测试.png"
+filePath = r"C:\Users\Administrator\Desktop\未命名.jpg"
 uploadURL = r"http://rc.hanvon.com/file/single/upload"
 url = r"http://rc.hanvon.com/rc/rapid"
 
-# def uploadWithUrllib(url,filePath):
-# 	with request.urlopen(url=uploadURL,data=getdata("file",filePath).encode("utf-8")) as page:
-# 		page.read().decode("utf-8")
-
-def upload(url,filePath):
-	cookies = {
-		"JSESSIONID":"DFC2729024CB6D75C4FAC86FB60E7B14",
-		"Hm_lvt_4c8be966697649d1191e0cfabcb9ea64":"1474852818,1474879212,1474951731,1475030808",
-		"Hm_lpvt_4c8be966697649d1191e0cfabcb9ea64":"1475032556"
-	}
-
-	files = {"file": open(filePath, "rb")}
-	r = requests.post(url=uploadURL,files=files,cookies=cookies)
-	print(r.text)
-
-upload(url,filePath)
+boundary = "---------------------------273761636013558"
 
 
-# def getdata(fileFieldName,filePath):
-# 	# 随即生成boundary
-# 	boundary = "--------sgc" + uuid.uuid4().hex;
-# 	# 获取文件名
-# 	fileName = os.path.basename(filePath)
-# 	# 解码为utf-8
-# 	# fileName = fileName.encode("utf-8")
-# 	# 获取文件类型
-# 	fileType = mimetypes.guess_type(fileName)[0]
-# 	# 如果没有该类型 则按文本类型处理
-# 	if fileType==None:
-# 		fileType = "text/plain; charset=utf-8"
+def uploadWithUrllib(url, filePath):
+    req = request.Request(url, getdata("file", filePath))
+    # 之前BUG 不添加 这一个行 便不可以
+    req.add_header('Content-Type', "multipart/form-data; boundary=" + boundary)
+    response = request.urlopen(req)
+    print(response.read().decode("utf-8"))
 
-# 	print(fileType)
-# 	print(fileName)
-# 	# 读取文件
-# 	fileData = open(filePath, "rb").read()
-# 	CRLF = "\r\n"
-# 	# 拼接body
-# 	body = ""
-# 	body += "--" + boundary + CRLF;
-# 	body += 'Content-Disposition: form-data; name="' + fileFieldName +'"; filename="' + fileName + '"'
-# 	body += 'Content-Type:' + fileType + CRLF
-# 	body += CRLF;
-# 	body += str(fileData) + CRLF
-# 	body += "--" + boundary + "--" + CRLF
-# 	print("body size:{0}".format(len(body)))
-# 	return body
 
-# uploadWithUrllib(url,filePath)
+def getdata(fileFieldName, filePath):
+    # 随即生成boundary
+
+    # 获取文件名
+    fileName = os.path.basename(filePath)
+    # 获取文件类型
+    fileType = mimetypes.guess_type(fileName)[0]
+    # 如果没有该类型 则按文本类型处理
+    if fileType == None:
+        fileType = "text/plain; charset=utf-8"
+    # 读取文件
+    fileData = open(filePath, "rb").read()
+    CRLF = "\r\n"
+    # 拼接body
+    head = ""
+    head += "--" + boundary + CRLF
+    head += 'Content-Disposition: form-data; name="' + \
+        fileFieldName + '"; filename="' + fileName + '"' + CRLF
+    head += 'Content-Type: ' + fileType + CRLF
+    head += CRLF
+    body = head.encode("utf-8") + fileData
+    tail = CRLF + "--" + boundary + "--" + CRLF
+    body += tail.encode("utf-8")
+    print(head, end="这是data")
+    print(tail)
+    return body
+
+uploadWithUrllib(uploadURL, filePath)
